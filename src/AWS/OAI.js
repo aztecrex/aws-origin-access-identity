3a4266de4a955b9e41103873adf67d7ac01fe1a4
Greg Wiley <aztec.rex@jammm.com>
2017-02-19 15:57:08 -0800

    error handling

--- src/AWS/OAI.js
+++ src/AWS/OAI.js
@@ -2,15 +2,23 @@
 
 
 function doNativeRequest(s, cb) {
-  setTimeout(function() { cb(s); }, 1000);
+  if (s === "Boink")
+    setTimeout(function() { cb(new Error("my word!")); }, 500);
+  else
+    setTimeout(function() { cb(null, s); }, 1000);
 }
 
-exports.createOAIImpl = function(callback) {
-  return function(request) {
-    return function() {
-      doNativeRequest(request, function(response) {
-        callback(response)();
-      });
+exports.createOAIImpl = function (cberror) {
+  return function (cbsuccess) {
+    return function (request) {
+      return function () {
+        doNativeRequest(request, function(err, succ) {
+          if (err)
+            cberror(err)();
+          else
+            cbsuccess(succ)();
+        });
+      }
     }
   }
 }
--- src/AWS/OAI.purs
+++ src/AWS/OAI.purs
@@ -1,20 +1,29 @@
-module OAI (createOAI, tryIt) where
+module OAI (createOAI, tryIt, tryIt2) where
 
 import Prelude (Unit, bind, ($), (<>))
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Aff (Aff, makeAff)
-import Control.Monad.Aff.Console(log, CONSOLE)
+import Control.Monad.Eff.Exception(Error)
+import Control.Monad.Aff (Aff, makeAff, attempt)
+import Control.Monad.Aff.Console(logShow, log, CONSOLE)
 
 foreign import createOAIImpl :: forall eff.
+  (Error -> Eff eff Unit) ->
   (String -> Eff eff Unit) ->
   String ->
   Eff eff Unit
 
 createOAI :: forall eff. String -> Aff eff String
-createOAI req = makeAff (\error success -> createOAIImpl success req)
+createOAI req = makeAff
+  (\error success -> createOAIImpl error success req)
 
 tryIt :: forall eff. Aff (console :: CONSOLE | eff) Unit
 tryIt = do
   out <- createOAI "Hi!"
   log ("got: " <> out)
+
+tryIt2 :: forall eff. Aff (console :: CONSOLE | eff) Unit
+tryIt2 = do
+  log "wot?"
+  out <- attempt $ createOAI "Boink"
+  logShow out
